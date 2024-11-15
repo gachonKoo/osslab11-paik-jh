122b461a8eaa897e8edd7d4c2cacf048be57e7ec
paik-jh <whitejh2004@naver.com>
2024-11-15 21:51:16 +0900

    code

--- geo/utils.py
+++ geo/utils.py
@@ -1,8 +1,6 @@
-
 import math
 
 def calculate_circle_properties(radius):
-    """Calculate the circumference and area of a circle."""
-    c = 2 * math.pi * radius  
-    area = math.pi * radius ** 2  
+    c = 2 * math.pi * radius
+    area = math.pi * radius ** 2
     return c, area
